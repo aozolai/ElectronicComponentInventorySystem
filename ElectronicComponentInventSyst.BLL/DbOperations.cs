bf911e8c3a3ee55dae38d5dedf03de2a2a75b581
Aija <a.ozolai@gmail.com>
2021-12-12 23:04:53 +0200

    DbOperations update 12/12/2021

--- ElectronicComponentInventSyst.BLL/DbOperations.cs
+++ ElectronicComponentInventSyst.BLL/DbOperations.cs
@@ -14,9 +14,36 @@ namespace ElectronicComponentInventSyst.BLL
         {
             _context = context;
         }
+        public void AddPart(ElectronicComponents electronicComponent)
+        {
+            _context.Add(electronicComponent);
+            _context.SaveChanges();
+        }
         public IEnumerable<ElectronicComponents> GetComponents()
         {
             return _context.ElectronicComponents;
+        }
+        public ElectronicComponents GetComponentById(int id)
+        {
+            return _context.ElectronicComponents.Find(id);
+        }
+        public IEnumerable<ElectronicComponents> GetComponentByName(string name)
+        {
+            return _context.ElectronicComponents.Where(x => x.Name == name);
+        }
+        public void UpdateComponent(ElectronicComponents electronicComponent)
+        {
+            _context.Update(electronicComponent);
+            _context.SaveChanges();
+        }
+        public void RemoveComponent(int id)
+        {
+            var electronicComponent = GetComponentById(id);
+            if (electronicComponent != null)
+            {
+                _context.ElectronicComponents.Remove(electronicComponent);
+                _context.SaveChanges();
+            }
         }
     }
 }
