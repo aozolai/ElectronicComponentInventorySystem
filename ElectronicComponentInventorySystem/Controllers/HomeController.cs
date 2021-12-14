3195d28f98fbdc2810d0ae5722363f8cea925eb4
Aija <a.ozolai@gmail.com>
2021-12-14 22:58:00 +0200

    ComponentList - saraksta attēlošana, dzēšana, labošana, meklēšana

--- ElectronicComponentInventorySystem/Controllers/HomeController.cs
+++ ElectronicComponentInventorySystem/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using ElectronicComponentInventorySystem.Models;
+﻿using AutoMapper;
+using ElectronicComponentInventorySystem.Models;
 using ElectronicComponentInventSyst.BLL;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -14,16 +15,18 @@ namespace ElectronicComponentInventorySystem.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly DbOperations _operations;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger, DbOperations operations)
+        public HomeController(ILogger<HomeController> logger, DbOperations operations, IMapper mapper)
         {
             _logger = logger;
             _operations = operations;
+            _mapper = mapper;
         }
 
         public IActionResult Index()
         {
-            var components= _operations.GetComponents();
+            var components = _operations.GetComponents();
             return View(components);
         }
 
@@ -35,7 +38,48 @@ namespace ElectronicComponentInventorySystem.Controllers
         public IActionResult ComponentList()
         {
             var components = _operations.GetComponents();
-            return View();
+            var viewModel = new UI.Models.FilterViewModel()
+            {
+                FoundComponents = _mapper.Map<IEnumerable<UI.Models.ElectronicComponentsModel>>(components)
+            };
+            return View(viewModel);
+        }
+
+        public IActionResult DeleteComponent(int id)
+        {
+            _operations.RemoveComponent(id);
+            return RedirectToAction("ComponentList");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var component = _operations.GetComponentById(id);
+            var viewModel = _mapper.Map<UI.Models.ElectronicComponentsModel>(component);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(UI.Models.ElectronicComponentsModel viewModel)
+        {
+            var component = _mapper.Map<ElectronicComponentInventSyst.Entity.ElectronicComponents>(viewModel);
+            _operations.UpdateComponent(component);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult ComponentList(UI.Models.FilterViewModel viewModel)
+        {
+            IEnumerable<ElectronicComponentInventSyst.Entity.ElectronicComponents> components;
+            if (string.IsNullOrWhiteSpace(viewModel.Search))
+            {
+                components = _operations.GetComponents();
+            }
+            else
+            {
+                components = _operations.GetComponentByName(viewModel.Search);
+            }
+            viewModel.FoundComponents = _mapper.Map<IEnumerable<UI.Models.ElectronicComponentsModel>>(components);
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
---
+++ ElectronicComponentInventorySystem/Models/FilterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectronicComponentInventorySystem.UI.Models
+{
+    public class FilterViewModel
+    {
+        public string Search { get; set; }
+        public IEnumerable<ElectronicComponentsModel> FoundComponents { get; set; }
+    }
+}
